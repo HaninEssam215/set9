701269d6e3f8519d25d8e7256611b1899ff05356
Hanin Essam <he106400@gmail.com>
2023-07-27 05:51:34 +0300

    Reminder initial layout

---
+++ PDFKIT.py
@@ -0,0 +1,17 @@
+import pdfkit
+
+path_to_file = 'reminder.html'
+
+
+options = {
+    'page-width': '8.27in', 
+    'page-height': '11.69in', 
+    'dpi': 100,
+    'margin-top': '0',
+    'margin-right': '0',
+    'margin-bottom': '0',
+    'margin-left': '0',
+    'enable-local-file-access': True,
+}
+
+pdfkit.from_file(path_to_file, 'reminder_output.pdf', options=options)
